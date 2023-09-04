63726c3d4173c47ce0c8e49be4f319b392d00123
Herwans <herwansharvel@gmail.com>
2023-09-04 22:33:47 +0200

    config: setup fill completion

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='hcleanerlib',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The library for the HCleaner GUI and HCleaner CLI projects',
+    author="Herwans Harvel",
+    license='MIT'
+)
