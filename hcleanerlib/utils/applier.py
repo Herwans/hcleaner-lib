a1a5c2e6fa91588e52332ea4d440d43535086872
Herwans <herwansharvel@gmail.com>
2023-09-04 23:04:47 +0200

    feat: create applier class

---
+++ hcleanerlib/utils/applier.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+from hcleanerlib.utils.config import Configuration
+
+
+class Applier:
+    def __init__(self, config_type):
+        self.__config = Configuration(config_type)
+
+    def apply_folder_rules(self, element):
+        altered_element = element
+        for rule in self.__config.get_folder_rules():
+            # 0 = Pattern
+            # 1 = Replacement
+            altered_element = re.sub(rule[0], rule[1], altered_element)
+        altered_element = altered_element.strip()
+        return altered_element
+
+    def apply_file_rules(self, element):
+        sets = self.__config.get_extension_sets()
+        file_name, file_extension = os.path.splitext(element)
+        altered_element = file_name
+        for rule in self.__config.get_file_rules():
+            if len(rule) == 4:
+                exclusion = rule[3].split(',')
+            else:
+                exclusion = []
+            if (rule[2] == '*' or file_extension.lstrip(".") in sets[rule[2]]) \
+                    and file_extension.lstrip(".") not in exclusion:
+                altered_element = altered_element.strip()
+                altered_element = re.sub(rule[0], rule[1], altered_element)
+        altered_element = altered_element.strip()
+
+        if altered_element == "":
+            return element
+        return altered_element + file_extension
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='hcleanerlib',
     packages=find_packages(),
+    setup_requires=['Pillow'],
     version='0.1.0',
     description='The library for the HCleaner GUI and HCleaner CLI projects',
     author="Herwans Harvel",
