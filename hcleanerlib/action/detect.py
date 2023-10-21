aa4982ef6901082cea4cc6c790655a6665416957
Herwans <herwansharvel@gmail.com>
2023-10-21 02:59:05 +0200

    refac: refactor detect and add test

--- hcleanerlib/action/detect.py
+++ hcleanerlib/action/detect.py
@@ -1,40 +1,47 @@
-import logging
-
 from hcleanerlib.utils.applier import Applier
 from hcleanerlib.utils.path import Path
 
 
 class Detect:
+    """Detect when cleaned elements of the folder have the same name."""
+
     def __init__(self, config_type):
         self.__applier = Applier(config_type)
 
-    def exec(self, folder):
-        """Detect when elements have the same name after cleaning"""
-        folder = Path(folder)
+    def exec(self, source_folder_path):
+        source_folder = Path(source_folder_path)
 
+        cleaned_folders = {}
+        cleaned_files = {}
         duplicate_folders = {}
         duplicate_files = {}
 
-        for sub_folder in folder.folders(True):
+        # Clean each folder and create a key values entry for each of them
+        # The key is the cleaned name
+        # Don't check yet if it has duplication
+        for sub_folder in source_folder.folders(True):
             cleaned = self.__applier.apply_folder_rules(sub_folder)
-            if cleaned not in duplicate_folders.keys():
-                duplicate_folders[cleaned] = []
-            duplicate_folders[cleaned].append(sub_folder)
+            if cleaned not in cleaned_folders.keys():
+                cleaned_folders[cleaned] = []
+            cleaned_folders[cleaned].append(sub_folder)
 
-        for file in folder.files(True):
+        # Do the same as the previous one, but with file
+        for file in source_folder.files(True):
             cleaned = self.__applier.apply_file_rules(file)
-            if cleaned not in duplicate_folders.keys():
-                duplicate_folders[cleaned] = []
-            duplicate_folders[cleaned].append(file)
+            if cleaned not in cleaned_files.keys():
+                cleaned_files[cleaned] = []
+            cleaned_files[cleaned].append(file)
 
-        for title, elements in duplicate_folders.items():
-            if len(elements) > 1:
-                yield title
-                for element in elements:
-                    yield "> " + element
+        # Filter to get only duplicate folders
+        for key, value in cleaned_folders.items():
+            if len(value) > 1:
+                duplicate_folders[key] = value
 
-        for title, elements in duplicate_files.items():
-            if len(elements) > 1:
-                yield title
-                for element in elements:
-                    yield "> " + element
+        for key, value in cleaned_files.items():
+            if len(value) > 1:
+                duplicate_files[key] = value
+
+        return {
+            "folders": duplicate_folders,
+            "files": duplicate_files
+        }
---
+++ tests/action/test_detect.py
@@ -0,0 +1,26 @@
+import unittest
+
+from hcleanerlib.action.detect import Detect
+from hcleanerlib.utils.applier import Applier
+from hcleanerlib.utils.config import Configuration
+
+
+class TestDetect(unittest.TestCase):
+    def test_exec(self):
+        detect = Detect("test")
+        result = {
+            "folders": {
+                "folder 1": [
+                    "folder 1 [test]",
+                    "[test] folder 1"
+                ]
+            },
+            "files": {
+                "file 1.txt": [
+                    "file 1 [test].txt",
+                    "[test] file 1.txt"
+
+                ]
+            }
+        }
+        self.assertEqual(result, detect.exec("..\\sample"))
